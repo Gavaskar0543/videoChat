8207fd29ca8d8e9d0cf41e5a1e79cf3dfe14061a
Gavaskar <103166821+Gavaskar0543@users.noreply.github.com>
2023-09-03 12:14:28 +0000

    togle setup done

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 let camera = document.getElementById('cameraFeed');
-navigator.mediaDevices.getUserMedia({ video: true })
-    .then(stream => {
-        camera.srcObject = stream;
-    })
-    .catch(error => {
-        console.error('Error accessing the camera:', error);
-    });
+let toggle = false;
+
+document.getElementById('callbtn').onclick = () => {
+    toggle = !toggle; // Toggle the flag
+    if (toggle) {
+        // Turn on the camera
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(stream => {
+                camera.srcObject = stream;
+            })
+            .catch(error => {
+                console.error('Error accessing the camera:', error);
+            });
+    } else {
+        // Turn off the camera
+        const stream = camera.srcObject;
+        if (stream) {
+            const tracks = stream.getTracks();
+            tracks.forEach(track => track.stop());
+            camera.srcObject = null;
+        }
+    }
+    alert("Call toggled!");
+};
